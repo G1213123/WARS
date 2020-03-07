287f5ab7ac73ae0a7b6e1704a19f912688e769be
G1213123 <49609970+G1213123@users.noreply.github.com>
2020-03-07 15:22:21 +0800

    Update data_gov.py

--- data_gov.py
+++ data_gov.py
@@ -9,12 +9,18 @@ import geopandas as gpd
 import webbrowser
 import pyproj
 
-# point for temp. loading of database
+# pointer for temp. loading of database
 global cache
 
 
 class map_gov:
+    '''
+    Creating a Web based map using folium module for logging the assessed area
+    '''
     def __init__(self, **kwargs):
+        '''
+        Define base map with area centroid focused
+        '''
         self.__slots__ = ['stops', 'aoi']
         self.__dict__.update( kwargs )
 
@@ -23,12 +29,15 @@ class map_gov:
         self.map_stop()
         self.map_aoi()
 
-        self.m.save( 'test.html' )
+        self.m.save( 'test.html' ) # save path to be finalized
 
         if True:
             webbrowser.open( 'test.html' )
 
     def map_stop(self):
+        '''
+        Adding markers for bus stops in the aoi
+        '''
         for key, b_stop in self.stops.iterrows():
             description = folium.Popup( html='<b>#' + str( key ) + '</b><br><font size="4">' + str(
                 b_stop['stop_name'] ) + '</font><br><i> lat=%s lon=%s </i>'
@@ -37,17 +46,34 @@ class map_gov:
                            popup=description, color='#3186cc', fill_color='#3186cc' ).add_to( self.m )
 
     def map_aoi(self):
-
+        '''
+        Adding area polygon to the map
+        '''
         polygon_geom = gpd.GeoDataFrame( index=[0], geometry=[self.aoi] )
         polygon_geom.crs = {'init': 'epsg:4326'}
         folium.GeoJson( polygon_geom ).add_to( self.m )
 
 
 def pt_in_polygon(x, y, polygon):
+    '''
+    Check a point in a polygon
+    '''
     return polygon.contains( Point( x, y ) )
 
 class data_gov:
+    '''
+    A frame work for searching of bus stop data using data.gov.hk as data source
+    For documentation of source data format, see https://static.data.gov.hk/td/pt-headway-en/dataspec/ptheadway_dataspec.pdf
+    Data is pre-cached in a cache folder if it is first time setup for subsequential query
+    Bus Stop Data querying can be in form of field value or location filtering. See read_by_field and 
+        read_by_loc respectively
+    Route list querying is presumed to be according to given area. See ruote_query
+    '''
     def __init__(self):
+        '''
+        Check first time setup
+        if file not exist, download the file as cache for later operations
+        '''
         self.cache_path = os.path.expanduser('~/Documents/Python_Scripts/WARS/cache')
         self.fields=['stops', 'stop_times', 'trips', 'routes']
         url='https://static.data.gov.hk/td/pt-headway-en/'
